916439a0eb7789b96ae102c6d87850f5a2ef2513
victor barbier <victor.barbier@recherche.gouv.fr>
2024-01-16 14:13:43 +0100

    create vite template with vosviewer

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// @ts-expect-error no types defined for vosviewer
+import { VOSviewerOnline } from 'vosviewer-online';
+import data from './JOI_2007-2016_co-authorship_network_template.json';
+
+function App() {
+  return (
+    <div style={{
+      position: 'absolute', top: '100px', left: '100px', width: '1100px', height: '700px', border: 'lightpink solid 5px'
+    }}>
+      <VOSviewerOnline data={data} parameters={{ scale: 1.0 }} />
+    </div>
+  );
+}
+
+export default App;
